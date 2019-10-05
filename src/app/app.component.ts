fa8024e12eb0924af2194bce04503d4732ee68cb
Radytin <Kreetta.hamalainen@helsinki.fi>
2019-10-05 22:34:51 +0300

    setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
 
 @Component({
   selector: 'app-root',
